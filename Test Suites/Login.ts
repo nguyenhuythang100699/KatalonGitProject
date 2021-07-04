f6c68898f73231fc77197f145b474f8a577e96a4
hoang <hoang@gmail.com>
2021-07-04 16:21:26 +0700

    Change for test on Jenkins for Git

---
+++ Test Suites/Login.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>17dec2aa-c323-45cf-a9e4-b4c04b5eda0b</testSuiteGuid>
+   <testCaseLink>
+      <guid>056f6ca3-cece-4cac-b2e5-616b0ebbf0ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pages/Login/TC01-VerifyErrorMsgWhenPassWrong</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>37817c1a-959c-4d7b-a268-028fa861b1d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pages/Login/TC02-VerifySucessfulMsgWhenValidAccount</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
