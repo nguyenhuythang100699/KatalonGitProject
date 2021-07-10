8ee1776feebc289e859d8b8e94625027d649fae3
hoang <hoang@gmail.com>
2021-07-10 15:23:21 +0700

    test again

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -4,7 +4,7 @@
    <name>Login</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
+   <mailRecipient>17110367@student.hcmute.edu.vn;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
